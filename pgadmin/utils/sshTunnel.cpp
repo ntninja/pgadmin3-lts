53f458300521b6a864c170e40b1e9226c4ff11b0
Erin of Yukis <erin-dev@ninetailed.ninja>
2025-08-31 21:19:20 +0200

    Merge https://cgit.freebsd.org/ports/tree/databases/pgadmin3/files/patch-pgadmin_utils_sshTunnel.cpp to build on clang

--- pgadmin/utils/sshTunnel.cpp
+++ pgadmin/utils/sshTunnel.cpp
@@ -19,7 +19,9 @@
 #include "utils/sshTunnel.h"
 #include "frm/frmMain.h"
 
+#ifdef WIN32
 #pragma comment (lib, "Ws2_32.lib")
+#endif
 
 typedef const char *(*inet_ntop_t) (int af, const void *src, char *dst, socklen_t size);
 
